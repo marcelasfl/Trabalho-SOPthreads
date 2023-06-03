9ed78a7917b19f0aeb7238a59ac8f0a3fa7903c4
Fatasy <marcelo.fatasy@hotmail.com>
2023-06-03 16:33:56 -0300

    add serial method

--- Pthreads.cpp
+++ Pthreads.cpp
@@ -1,11 +1,17 @@
-#pragma once
-
 #define HAVE_STRUCT_TIMESPEC
-#include <pthread.h> 
+#include <pthread.h>
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "helps.h"
+
+
+void countNumberPrimesInMatrizWithSerialMethod(int** matriz) {
+	int primeNumbersInMatriz = countPrimesInMatriz(matriz);
+	printf("Number of primes in matriz: %d\n", primeNumbersInMatriz);
+}
 
 int main() {
-	return 1;
+	int **matriz = genMatriz(GenMatrizParams{1000, 1000, 1, 31999});
+	countNumberPrimesInMatrizWithSerialMethod(matriz);
 }
